71af25e1a9b031840481f617faa824cdcfc3a3a8
Jim <62217728+wgithub1@users.noreply.github.com>
2022-05-17 16:37:49 -0400

    Plot dataframe
    Plot a dataframe as a 3D surface

---
+++ Exotic Greeks 2/plot_df_3d.py
@@ -0,0 +1,39 @@
+# Import libraries
+from mpl_toolkits import mplot3d
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+def plot_df_3d(df,xlabel='x',ylabel='y',zlabel='z',title='surface_plot'):
+    '''
+    Given a df, plot 3d surface of this df
+    '''
+    
+    
+    # Creating dataset
+    x = list(df.index)
+    y = list(df.columns)
+    X, Y = np.meshgrid(x, y)
+    Z=df.values
+    
+    # Creating figure
+    fig = plt.figure(figsize =(14, 9))
+    ax = plt.axes(projection ='3d')
+    
+    # Creating color map
+    my_cmap = plt.get_cmap('winter')
+    
+    # Creating plot
+    surf = ax.plot_surface(X, Y, Z,
+                        cmap = my_cmap,
+                        edgecolor ='none')
+    
+    fig.colorbar(surf, ax = ax,
+                shrink = 0.5, aspect = 5)
+    
+    ax.set_title(title)
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel(ylabel)
+    ax.set_zlabel(zlabel)
+    # show plot
+    plt.show()
